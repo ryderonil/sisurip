fdf63686011ebfeb68f8b4289b4cc30d1f28735a
mashtree@gmail.com <mashtree@gmail.com@b1aac15b-fe90-3c3e-5df6-4027e1396a07>
2013-05-09 01:26:14 +0000

    halaman grafik surat

--- trunk/views/monitoring/asalsurat.php
+++ trunk/views/monitoring/asalsurat.php
@@ -9,15 +9,16 @@ require 'libs/phplot/phplot.php';
 
 $data = $this->data;
 
-$plot = new PHPlot(800);
+$plot = new PHPlot(400,250);
 $plot->SetImageBorderType('plain');
-$plot->SetPlotType('bars');
+$plot->SetPlotType('thinbarline');
 
 $plot->SetDataType('text-data');
 $plot->SetDataValues($data);
 # Main plot title:
-$plot->SetTitle('');
+$plot->SetTitle('ASAL SURAT');
 $plot->SetBackgroundColor('#eeeeff');
+$plot->SetDataColors(array('blue'));
 # Make a legend for the 3 data sets plotted:
 # Turn off X tick labels and ticks because they don't apply here:
 $plot->SetXTickLabelPos('none');
---
+++ trunk/views/monitoring/grafik.php
@@ -0,0 +1,30 @@
+<table>
+    <tr width="100%">
+        <td>
+            <iframe class="grafik-rekap" align="center" 
+                    src="<?php echo URL; ?>monitoring/grafikTipeSuratKeluar" width="450" height="250">
+            <p align="center">data tidak ada</p>
+            </iframe>
+        </td>
+        <td>
+            <iframe class="grafik-rekap" align="center" 
+                    src="<?php echo URL; ?>monitoring/grafikJmlSuratMasukHarian" width="450" height="250">
+            <p align="center">data tidak ada</p>
+            </iframe>
+        </td>
+    </tr>
+    <tr width="100%">
+        <td>
+            <iframe class="grafik-rekap" align="center" 
+                    src="<?php echo URL; ?>monitoring/grafikAsalSurat" width="450" height="250">
+            <p align="center">data tidak ada</p>
+            </iframe>
+        </td>
+        <td>
+            <iframe class="grafik-rekap" align="center" 
+                    src="<?php echo URL; ?>monitoring/grafikJmlSuratKeluarHarian" width="450" height="250">
+            <p align="center">data tidak ada</p>
+            </iframe>
+        </td>
+    </tr>
+</table>
